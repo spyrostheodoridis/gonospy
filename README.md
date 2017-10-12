5bc1d5f2c3f00470653f13f352b43679588c0b13
Spyros <spyrostheodoridis@gmail.com>
2017-10-12 18:23:33 +0200

    updated functions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # gonospy
+
+The gonospy (gonos = offspring) package contains a set a functions for filtering, transforming and summarizing high-throughput genomic data. The current version focuses on
+data generated via sequencing of reduced representation libraries (RRLs, e.g. RAD-seq) using Illumina technology. Future versions will include alternative sequencing technologies as well.
--- gonospy/filterFunc.py
+++ gonospy/filterFunc.py
@@ -1,145 +1,125 @@
 import numpy as np
 import os
 from itertools import *
-import time
 
-start = time.time()
-
-def write_lines_to_file(outfile, line_list):
+def writeLinesToFile(outfile, line_list):
     # the file should be already opened!
     for item in line_list:
-        outfile.write("%s" % item)
+        outfile.write('%s' % item)
 
-#lines for each read/record
-n=4
+def filterReads(indir, qThresh = 30, pQ = 0.5, trmBases = 11, repThrs = 1000):
+    #lines for each read/record
+    n=4
+    # qThresh -> base quality threshold
+    # pQ -> percentage of the read to be checked for quality score
+    # trmBases -> number of bases to trim
+    # repThrs -> repetitive read threshold
 
-#Base quality threshold
-Q_thresh = 30
+    #define output directory
+    cleanFiles = '{}/cleanFiles'.format(indir)
 
-#percentage of the read to be checked for quality score
-p_Q = 0.5
+    #make output directory
+    os.mkdir(cleanFiles)
 
-#number of bases to trim
-trm_bases = 11
+    #get Illumina Q-scores
+    with open('{}/illumina_Q_scores_phred33.txt'.format(indir), 'r') as Qscores:
+        #dictionary to store symbol and value
+        illD = {}
+        for line in Qscores:
+            illD[line.split('\t')[0]] = line.split('\t')[1].rstrip()
 
-#repetitive read threshold
-rep_Thrs = 1000
+    listdir = os.listdir(indir)
+    nIndv = 0
 
-#define input directory
-indir = '/Volumes/3.6_TB_LaCie/study_2/seq/test'
+    #log_file
+    flog = open(os.path.join(cleanFiles,'trimmLog.txt'), 'w')
+    flog.write('\t'.join(['sample', 'total_reads', 'lowQ_reads_discarded','Repetative_reads_discarded', 'Number_of_repetative_stacks', '\n']))
 
-#define output directory
-clean_files = '/Volumes/3.6_TB_LaCie/study_2/seq/test/clean_files'
+    for fname in listdir:
+        if fname.endswith('fastq.gz'):
+            os.system('gunzip -k {}/{}'.format(indir, fname))
+            
+            print('Processing file %s' %fname)
+            fileInfo = []
+            totalReads = 0
+            lowQreads = 0
+            repReads = 0
+            repStacks = 0
+            nIndv += 1
+            infile = "{}/{}".format(indir, '.'.join(fname.split('.')[:-1]))
+            with open(infile, 'r') as f:
+                
+                dic = {} #dictionary to store the id's as keys and read and quality as values
+                countD = {} #dictionary to store counts (coverage) for each read
+                while True:
+                    nextLines = list(islice(f, n))
+                    if not nextLines:
+                        break
 
-#make output directory
-os.mkdir(clean_files)
-
-#get Illumina Q-scores
-with open("./illumina_Q_scores_phred33.txt", "r") as Qscores:
-    #dictionary to store symbol and value
-    ill_d = {}
-    for line in Qscores:
-        ill_d[line.split("\t")[0]] = line.split("\t")[1].rstrip()
-        
-#get population codes for individuals
-with open("./popmap_codes.txt", "r") as PopCodes:
-    Pop_D = {}
-    for line in PopCodes:
-        Pop_D[line.split("\t")[0]] = line.split("\t")[1]
-
-listdir = os.listdir(indir)
-n_indv = 0
-
-#log_file
-flog = open(os.path.join(clean_files,'trimm_log.txt'), 'w')
-flog.write('\t'.join(['sample', 'total_reads', 'lowQ_reads_discarded','Repetative_reads_discarded', 'Number_of_repetative_stacks', '\n']))
-
-for fname in listdir:
-    if fname.startswith('pf') and fname.endswith("fastq"):
-        #os.system('sudo gunzip -k %s' %fname)
-        print('Processing file %s' %fname)
-        file_info = []
-        total_reads = 0
-        low_Q_reads = 0
-        repReads = 0
-        repStacks = 0
-        n_indv += 1
-        
-        extrFile = fname#[:-3]
-        with open(os.path.join(indir, extrFile), 'r') as f:
-            dic = {} #dictionary to store the id's as keys and read and quality as values
-            countD = {} #dictionary to store counts (coverage) for each read
-            while True:
-                next_n_lines = list(islice(f, n))
-                if not next_n_lines:
-                    break
+                    totalReads += 1
+                    #get only the read sequence and trim it
+                    rowRead = nextLines[1].rstrip()
+                    read = rowRead[:-trmBases]
+                    readLen = len(read)
+                    #get the quality of the trimmed read
+                    readQual = nextLines[3].rstrip()[:-trmBases]
                     
-                total_reads += 1
-                #get only the read sequence and trim it
-                row_read = next_n_lines[1].rstrip()
-                read = row_read[:-trm_bases]
-                readLen = len(read)
-                #get the quality of the trimmed read
-                read_Qual = next_n_lines[3].rstrip()[:-trm_bases]
-                #get read ID
-                read_ID = next_n_lines[0].rstrip()
-                
-                #mark/discard reads with at least one uncalled base. '#' is for the uncalled base
-                if '#' in read_Qual:
-                    hiQRead = False
-                else:
-                    hiQRead = True
-                
-                #use the ill_d dictionary to convert ascii to integer scores
-                Q_int = [int(ill_d[i]) for i in read_Qual]
-                
-                #check the quality of the first p_Q% of the reads
-                Q_sub = Q_int[0:(int(p_Q*readLen))]
-                #check if two thirds of the bases are above the Q threshold
-                n_bases_highQ = 0
-                for score in Q_sub:
-                    if score >= Q_thresh:
-                        n_bases_highQ += 1
-                
-                #get only reads with a mean quality score above the threshold
-                if n_bases_highQ >= (2/3)*(len(Q_sub)) and hiQRead == True:
-                    dic[read_ID] = [read, read_Qual]
-                    #count coverage
-                    if read in countD:
-                        #increase number of reads by 1
-                        countD[read] += 1
+                    #get read ID
+                    readID = nextLines[0].rstrip()
+
+                    #mark/discard reads with at least one uncalled base. '#' is for the uncalled base
+                    if '#' in readQual:
+                        hiQRead = False
                     else:
-                        nOfReads = 1
-                        countD[read] = nOfReads
-                        
-                else:
-                    low_Q_reads += 1
-                    
-            #open file to write filtered data
-            popCode = Pop_D[extrFile[0:-6]]
-            fullName = '_'.join([extrFile[0:-6], popCode])
-            fout = open(os.path.join(clean_files,'%s.fastq' %fullName), 'w')
-            
-            for key, value in dic.items(): #for each read in the dictionary
-                readStr = value[0]
-                read_Quality = value[1]
-                #if read is low copy
-                if countD[readStr] <= rep_Thrs:
-                    read_lines = [key, '\n', readStr, '\n', '+\n', read_Quality, '\n']
-                    write_lines_to_file(fout, read_lines)
-                
-            #get statistics for highly repetitive reads
-            for read, count in countD.items():
-                if count >= rep_Thrs:
-                    repReads += count
-                    repStacks += 1
-            
-            fout.close()
-            
-            log_list = [str(fullName),str(total_reads),str(low_Q_reads),str(repReads), str(repStacks), '\n']
-            flog.write('\t'.join(log_list))
-            flog.flush()
-            print('Processed Sample %s (%s) \nTotal number of reads: %s, LowQ Reads discarded: %s, Highly Repetitive Reads discarded: %s' %(fullName, n_indv, total_reads, low_Q_reads, repReads))
-            os.system('sudo rm %s' %extrFile)
-print('Finished\n')
-flog.close()
+                        hiQRead = True
+
+                    #use the illD dictionary to convert ascii to integer scores
+                    Qint = [int(illD[i]) for i in readQual]
+
+                    #check the quality of the first p_Q% of the reads
+                    Qsub = Qint[0:(int(pQ*readLen))]
+                    #check if two thirds of the bases are above the Q threshold
+                    nBasesHighQ = 0
+                    for score in Qsub:
+                        if score >= qThresh:
+                            nBasesHighQ += 1
+
+                    #get only reads with a mean quality score above the threshold
+                    if nBasesHighQ >= (2/3)*(len(Qsub)) and hiQRead == True:
+                        dic[readID] = [read, readQual]
+                        #count coverage
+                        if read in countD:
+                            #increase number of reads by 1
+                            countD[read] += 1
+                        else:
+                            nOfReads = 1
+                            countD[read] = nOfReads
+
+                    else:
+                        lowQreads += 1
+
+                fout = open(os.path.join(cleanFiles,'{}.fastq'.format(fname.split('.')[0])), 'w')
+
+                for key, value in dic.items(): #for each read in the dictionary
+                    readStr = value[0]
+                    readQuality = value[1]
+                    #if read is low copy
+                    if countD[readStr] <= repThrs:
+                        readLines = [key, '\n', readStr, '\n', '+\n', readQual, '\n']
+                        writeLinesToFile(fout, readLines)
+
+                #get statistics for highly repetitive reads
+                for read, count in countD.items():
+                    if count >= repThrs:
+                        repReads += count
+                        repStacks += 1
+
+                fout.close()
+
+                logList = [str(fname.split('.')[0]),str(totalReads),str(lowQreads),str(repReads), str(repStacks), '\n']
+                flog.write('\t'.join(logList))
+                flog.flush()
+                print('Processed Sample %s (%s) \nTotal number of reads: %s, LowQ Reads discarded: %s, Highly Repetitive Reads discarded: %s' %(fname.split('.')[0], nIndv, totalReads, lowQreads, repReads))
+            os.remove(infile)
+    print('Finished\n')
+    flog.close()
